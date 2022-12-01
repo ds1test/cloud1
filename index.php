4317a866c96d7198b2e2bfb9e155947755d2ba9c
ds1test <119607220+ds1test@users.noreply.github.com>
2022-12-01 20:29:21 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<div>
+<a href="http://www.oracle.com/?elqTrackId=6ACC3D3E374295DF936F1B9ACC93A501&elqTrack=true&myparam=<span class=eloquaemail encodeFor=none>First_Name11</span>">Clickme</a>
+<a href="http://mydomain.com?elqTrack=true">Link 1</a>
+<a href="http://mydomain.com?myparam=val1&elqTrack=true">Link 2</a>
+<a href="http://mydomain.com?elqTrack=true&myFirstName=<span class=eloquaemail>First_Name11</span>">Link 3</a>
+<a href="http://mydomain.com?myparam=val1&elqTrack=true&elqTrackId=6ACC3D3E374295DF936F1B9ACC93A501">Link 4</a>
+<a href="http://mydomain.com?myparam=val1&elqTrack=true#myanchor">Link 5</a>
+<a href="http://www.google.com">Google</a>
+<a href="ftp://ftp.support.unisys.com/pub/mcp/ide/Readme.txt">ftp</a>
+<a href="webcal://www.loringward.com/adSitedocs/YE_Tax_Center_Calendar_2013.ics">Webcal</a>
+<a href="/mysite/index.html">index1</a>
+<a href="../mysite/index.html">index2</a>
+<a href="index.html">index3</a>
+<a href="news.google.com/nwshp?hl=en">newgoogle</a>
+</div>
